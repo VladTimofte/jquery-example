b1029b1967228683d9218444fac20e3f6c4ff197
Vlad <vladtimofte999@gmail.com>
2024-09-22 10:22:05 +0300

    Student Grades app

---
+++ student-grades/app.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+  // Adauga studentul in lista
+
+  $("#grade-form").on("submit", function (event) {
+    event.preventDefault();
+
+    //Obtinem numele si nota studentului
+    const name = $("#student-name").val();
+    const grade = $("#student-grade").val();
+
+    const regex = /^[a-zA-Z]+$/;
+
+    if (regex.test(name)) {
+      //Verificam daca ambele campuri sunt completate
+      if (name && grade) {
+        // Adauga in lista
+        $("#students-list").append(`<li> ${name} - ${grade} </li>`);
+
+        // Reseteaza campurile formularului
+        $("#student-name").val("");
+        $("#student-grade").val("");
+      } else {
+        alert("Completeaza ambele campuri!");
+      }
+    } else {
+        alert('Numele studentului Nu trebuie sa containa cifre!')
+    }
+  });
+});
